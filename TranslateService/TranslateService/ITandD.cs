116b6664a29a8edef8b2cf71d61af4b18b3bdf73
Goncalo Vilhena <vilhena@gmail.com>
2011-11-17 19:17:43 +0000

    TranslateService

---
+++ TranslateService/TranslateService/ITandD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TranslateService
+{
+    [ServiceContract(Namespace = "webclientvilhena.apphb.com")]
+    public interface ITandD
+    {
+        [OperationContract]
+        MeaningResp TraslateAndMeaning(WordReq wordReq);
+    }
+
+    [DataContract(Namespace = "webclientvilhena.apphb.com")]
+    public class WordReq
+    {
+        [DataMember]
+        public string Word { get; set; }
+    }
+
+    [DataContract(Namespace = "webclientvilhena.apphb.com")]
+    public class MeaningResp
+    {
+        [DataMember]
+        public string OriginalWord { get; set; }
+        [DataMember]
+        public string TranslatedWord { get; set; }
+        [DataMember]
+        public string[] Meanings { get; set; }
+        [DataMember]
+        public bool Translated { get; set; }
+    }
+
+}
---
+++ TranslateService/TranslateService/TandD.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TranslateService.api.microsofttranslator.com;
+using TranslateService.services.aonaware.com;
+
+namespace TranslateService
+{
+    public class TandD : ITandD
+    {
+        private const string Apiid = "C1E6D88CE2967328BBA9BC6C932B9D177247CAE5";
+        private static readonly LanguageServiceClient LanguageServiceClient = new LanguageServiceClient();
+        private static readonly DictServiceSoapClient DictServiceSoapClient= new DictServiceSoapClient("DictServiceSoap12");
+
+        public MeaningResp TraslateAndMeaning(WordReq wordReq)
+        {
+            var resp = new MeaningResp {OriginalWord = wordReq.Word};
+
+            string translation = GetTranslation(wordReq);
+            resp.Translated = resp.OriginalWord != translation;
+            if (resp.Translated)
+            {
+                resp.TranslatedWord = translation;
+                resp.Meanings = GetMeans(translation);
+            }
+            return resp;
+        }
+
+        private static string[] GetMeans(string translation)
+        {
+            return DictServiceSoapClient.Define(translation)
+                .Definitions
+                .Select(definition => definition.WordDefinition)
+                .ToArray();
+        }
+
+        private static string GetTranslation(WordReq wordReq)
+        {
+            var languageService = new LanguageServiceClient();
+            return LanguageServiceClient.Translate(Apiid, wordReq.Word, "pt", "en", "text/html", "general");
+        }
+    }
+}
